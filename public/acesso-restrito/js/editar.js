78a221b232187cbacd12deff690225ebcbbb64f5
Vinicius Caetano <viniciuscaetanosilva@gmail.com>
2023-05-11 03:04:22 -0300

    fix-done-editusuario-layoutlista
    Eu arrumei o layout da lista e o botão pra deixar a tarefa como feita, a edição de usuario ta funcionando e adicionei alguns filtros principalmente pra evitar campo vazio ou poucos caracteres. Ainda preciso repetir os filtros em outros campos. Entrega é Hoje 11/05.

--- public/acesso-restrito/js/editar.js
+++ public/acesso-restrito/js/editar.js
@@ -3,25 +3,44 @@ var nomeg = localStorage.getItem("nameg");
 var emailg = localStorage.getItem("emailg");
 var usernameg = localStorage.getItem("usernameg");
 var passwordg = localStorage.getItem("passwordg");
-var nome = document.getElementById('nomecad')
-var email = document.getElementById('emailcad')
-var username = document.getElementById('nomescad')
-var password = document.getElementById('senhacad')
+var nome = document.getElementById('nomecad');
+var email = document.getElementById('emailcad');
+var username = document.getElementById('nomescad');
+var password = document.getElementById('senhacad');
 
 function carregara() {
-    let token = localStorage.getItem('token');
-    let nomeg = localStorage.getItem('nameg');
-    let emailg = localStorage.getItem('emailg');
     let usernameg = localStorage.getItem('usernameg');
     let passwordg = localStorage.getItem('passwordg');
 
-    let nome = document.getElementById('nomecad').value = nomeg;
-    let email = document.getElementById('emailcad').value = emailg;
-    let username = document.getElementById('nomescad').value = usernameg;
-    let password = document.getElementById('senhacad').value = passwordg;
+    const url = 'https://todolist-api.edsonmelo.com.br/api/user/login/';
+    const pay_load = {
+        'username': usernameg,
+        'password': passwordg
+    };
+    const headers = { 'Content-type': 'application/json', 'Accept': 'text/plain' };
+
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(pay_load),
+        headers: headers
+    })
+        .then(response => response.json())
+        .then(data => {
+            if ('message' in data) {
+                throw new Error(data['message']);
+            } else {
+                nome.value = data['name'];
+                email.value = data['email'];
+                username.value = usernameg;
+                password.value = passwordg;
+                localStorage.setItem('nameg', data['name']);
+                localStorage.setItem('emailg', data['email']);
+            }
+        })
+        .catch(error => console.log(error));
 }
 
-function editar() {
+function editardados() {
     let token = localStorage.getItem('token');
     let nome = document.getElementById('nomecad').value;
     let email = document.getElementById('emailcad').value;
@@ -34,7 +53,7 @@ function editar() {
         email: email,
         username: username,
         password: password,
-        picture: 'ddsmldmsldm' 
+        picture: 'ddsmldmsldm'
     };
 
     const headers = { 'Content-Type': 'application/json', 'Authorization': token };
@@ -44,15 +63,91 @@ function editar() {
         headers: headers,
         body: JSON.stringify(pay_load)
     })
-    .then(response => response.json())
-    .then(data => {
-        if ('message' in data) {
-            throw new Error(data['message']);
-            
-        } else {
-            console.log(data);
-            alert("Editado com sucesso!");
-        }
-    })
-    .catch(error => console.log(error));
+        .then(response => response.json())
+        .then(data => {
+            if ('message' in data) {
+                carregara();
+                throw new Error(data['message']);
+            } else {
+                console.log(data);
+                alert("Editado com sucesso!");
+            }
+        })
+        .catch(error => console.log(error));
 }
+
+function editaruser() {
+    if (filtrarDados()) {
+        let token = localStorage.getItem('token');
+        let new_username = document.getElementById('newusernamecad').value;
+        let new_password = document.getElementById('novasenhacad').value;
+        let username = document.getElementById('usernamecad').value;
+        let password = document.getElementById('senhacad').value;
+
+        const url = "https://todolist-api.edsonmelo.com.br/api/user/updateuserpass/";
+        const pay_load = {
+            username: username,
+            password: password,
+            new_username: new_username,
+            new_password: new_password
+
+        };
+
+        const headers = { 'Content-Type': 'application/json', 'Authorization': token };
+
+        fetch(url, {
+            method: 'PUT',
+            headers: headers,
+            body: JSON.stringify(pay_load)
+        })
+            .then(response => response.json())
+            .then(data => {
+                if ('message' in data) {
+                    if (data['message'] == 'User/password Successfully Updated') {
+                        alert('Usuário atualizado com sucesso')
+                    } else if (data['message'] == 'Incorrect username and/or password') {
+                        alert('Nome de usuário e/ou senha incorretos')
+                    }
+                    else {
+                        alert(data['message'])
+                        throw new Error(data['message']);
+                    }
+                } else {
+                    console.log(data);
+                    alert("Editado com sucesso!");
+                }
+            })
+            .catch(error => console.log(error));
+    }
+}
+
+function filtrarDados() {
+    const usuario = document.getElementById("usernamecad");
+    const novoUsuario = document.getElementById("newusernamecad");
+    const senha = document.getElementById("senhacad");
+    const novaSenha = document.getElementById("novasenhacad");
+    const confirmarNovaSenha = document.getElementById("confirmarnovasenhacad");
+
+    // Verifica se todos os campos obrigatórios foram preenchidos
+    if (!novoUsuario.value || !senha.value || !usuario.value || !novaSenha.value || !confirmarNovaSenha.value) {
+        alert("Por favor, preencha todos os campos obrigatórios.");
+        return false;
+    }
+
+    // Verifica se o campo Nome e o campo Nome de Usuário possuem pelo menos 3 caracteres
+    if (usuario.value.length < 3 || novoUsuario.value.length < 3) {
+        alert("Os campos Nome e Nome de Usuário devem conter pelo menos 3 caracteres.");
+        return false;
+    }
+
+    // Verifica se o valor do campo Senha é igual ao valor do campo Nova Senha
+    if (novaSenha.value !== confirmarNovaSenha.value) {
+        alert("A senha é diferente da ");
+        return false;
+    }
+
+    return true;
+}
+
+// Chamar a função para carregar os dados do usuário
+
--- public/acesso-restrito/lista.js
+++ public/acesso-restrito/lista.js
@@ -1,35 +1,36 @@
-    let input = document.getElementById("inputText");
-    let list = document.getElementById("list");
-    let minimalValue = 3;
-    let listNum = 0;
-    let token = localStorage.getItem("token");
-    
+let input = document.getElementById("inputText");
+let list = document.getElementById("list");
+let minimalValue = 3;
+let token = localStorage.getItem("token");
+var nomeg = localStorage.getItem("nameg");
+var navnome = document.getElementById("navnome");
 
-    let tasks = [];
+navnome.innerText = nomeg;
+let tasks = [];
 
-    function carregarLista(){
-        const url = 'https://todolist-api.edsonmelo.com.br/api/task/search/';
+function carregarLista() {
+    const url = 'https://todolist-api.edsonmelo.com.br/api/task/search/';
 
-        const headers = { 'Content-type': 'application/json', 'Authorization': token };
+    const headers = { 'Content-type': 'application/json', 'Authorization': token };
 
-        fetch(url, {
-            method: 'POST',
-            headers: headers,
-            body: null
-            }).then(response => {
-            // Aqui devem ser realizados os tratamentos no caso de ocorrerem erros
-            response.json().then(data => {
+    fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: null
+    }).then(response => {
+        // Aqui devem ser realizados os tratamentos no caso de ocorrerem erros
+        response.json().then(data => {
             if ('message' in data) {
-            // Gera uma mensagem de erro com o valor retornado pela API ou conexão
-            throw new Error(data.message);
+                // Gera uma mensagem de erro com o valor retornado pela API ou conexão
+                throw new Error(data.message);
             } else {
-            // Mostra os dados retornados já convertidos
-            for (let i = 0; i < data.length; i++) {
-            console.log(data[i].name);
-            list.innerHTML += ` <li class=" my-3 py-3 list-group-item " id="list${data[i].id}">
+                // Mostra os dados retornados já convertidos
+                for (let i = 0; i < data.length; i++) {
+                    if(data[i].realized == 0){
+                    list.innerHTML += ` <li class=" my-3 py-3 list-group-item " id="list${data[i].id}">
                     <div class="row">
                     <div class="col-1">
-                    <input class="" type="checkbox" id="check${listNum}" onclick="done(${data[i].name})">
+                    <input class="" type="checkbox" id="check${data[i].id}" onclick="done(${data[i].id})">
                     </div>
                     <div class="col-6">
                         <span class=" h5" id="text${data[i].id}"> ${data[i].name} </span>
@@ -40,51 +41,68 @@
                     </div>                  
                     </div>    
                     </li> `;
-                input.value = " ";
+                    input.value = " ";
+                }else{
+                    list.innerHTML += ` <li class=" my-3 py-3 list-group-item " id="list${data[i].id}">
+                    <div class="row">
+                    <div class="col-1">
+                    <input class="" type="checkbox" id="check${data[i].id}" onclick="done(${data[i].id})">
+                    </div>
+                    <div class="col-6">
+                        <span class=" h5 text-decoration-line-through" id="text${data[i].id}"> ${data[i].name} </span>
+                    </div>
+                    <div class="col-4">
+                        <i class=" fa fa-trash" style="cursor:pointer;" onclick="deleteList(${data[i].id})"></i>
+                        <i class="fa fa-thin fa-pencil" style="cursor:pointer;" onclick="editList(${data[i].id}, '${data[i].name}', ${data[i].realized})"></i>
+                    </div>                  
+                    </div>    
+                    </li> `;
 
+                }
 
-        }}}).catch(error => {
+                }
+            }
+        }).catch(error => {
             console.log(error);
-            });
-            }).catch(error => {
-            console.log(error);
-            });
-        }
-            
-        addList = () => {
-            let listNum = 0;
-            const url = 'https://todolist-api.edsonmelo.com.br/api/task/new/';
-            const headers = { 'Content-type': 'application/json', 'Authorization': token };
-        
-            // obter o valor do input e filtrar o texto
-            let inputText = filterList(input.value);
-            // verificar se o texto filtrado é válido
-            if (inputText) {
-                let newTask = {
-                    name: inputText,
-                };
-        
-                // Converter o objeto em JSON
-                const payload = JSON.stringify(newTask);
-        
-                fetch(url, {
-                    method: 'POST',
-                    headers: headers,
-                    body: payload
-                })
-                .then(response => response.json())
-                .then(data => {
-                    // Adicionar a nova tarefa na lista de tarefas
-                    newTask.id = data.id;
-                    tasks.push(newTask);
-                    // atualizar a lista de tarefas na interface
-                    list.innerHTML += ` <li class=" my-3 py-3 list-group-item " id="list${newTask.id}">
+        });
+    }).catch(error => {
+        console.log(error);
+    });
+}
+
+addList = () => {
+    const url = 'https://todolist-api.edsonmelo.com.br/api/task/new/';
+    const headers = { 'Content-type': 'application/json', 'Authorization': token };
+
+    // obter o valor do input e filtrar o texto
+    let inputText = filterList(input.value);
+    // verificar se o texto filtrado é válido
+    if (inputText) {
+        let newTask = {
+            name: inputText,
+        };
+
+        // Converter o objeto em JSON
+        const payload = JSON.stringify(newTask);
+
+        fetch(url, {
+            method: 'POST',
+            headers: headers,
+            body: payload
+        })
+            .then(response => response.json())
+            .then(data => {
+                // Adicionar a nova tarefa na lista de tarefas
+                newTask.id = data.id;
+                tasks.push(newTask);
+                // atualizar a lista de tarefas na interface
+                list.innerHTML += ` <li class=" my-3 py-3 list-group-item " id="list${newTask.id}">
                         <div class="row">
                         <div class="col-1">
                         <input class="" type="checkbox" id="check${newTask.id}" onclick="done(${newTask.id})">
                         </div>
                         <div class="col-6">
-                            <span class=" h5" id="text${newTask.id}"> ${newTask.name} </span>
+                            <span class=" h5 " id="text${newTask.id}"> ${newTask.name} </span>
                         </div>
                         <div class="col-4">
                             <i class=" fa fa-trash" style="cursor:pointer;" onclick="deleteList(${newTask.id})"></i>
@@ -92,128 +110,158 @@
                         </div>                  
                         </div>    
                         </li> `;
-                    
-                    // limpar o input e atualizar o número de tarefas
-                    console.log(newTask);
-                    input.value = "";
-                    listNum++;
-                    location.reload();
-                })
-                .catch(error => console.error(error));
-            }
-        };
-          
-        /* Verificar se o item foi concluído */
-        done = (listId) => {
-            let checkbox = document.getElementById(`check${listId}`);
-            let current = document.getElementById(`text${listId}`);
-            let classExit = current.classList.contains("text-decoration-line-through");
-            if (classExit == true) {
-                current.classList.remove("text-decoration-line-through");
-            } else {
-                current.classList.add("text-decoration-line-through");
-            }
 
-        }
-        /*Verificar se tem mais de 3 caractéres */
-        filterList = (x) => {
-            if (x) {
-                if (x.length >= minimalValue) {
-                    return x;
-                }
-                else {
-                    alert("Digite mais de 3 caractéres no nome da Tarefa")
-                }
-            }
-            else {
-                return false;
-            }
-        }
-        /*Alerta de Edição de Tarefa*/
-        editList = (listId, listName, listDone) => {
-            let current = document.getElementById(`text${listId}`).innerHTML;
-            let newText = prompt(`Deseja alterar o nome da Tarefa? ${current}`);
-            const url = 'https://todolist-api.edsonmelo.com.br/api/task/update/';
-            const headers = { 'Content-type': 'application/json', 'Authorization': token };
-            const pay_load = {
-                id: listId,
-                name: newText,
-                realized: listDone,
-            };
-        
-            if (filterList(newText)) {
-                fetch(url, {
-                    method: 'PUT',
-                    headers: headers,
-                    body: JSON.stringify(pay_load),
-                })
-                .then(response => {
-                    if (!response.ok) {
-                      throw new Error(response.status);
-                    }
-                    return response.json();
-                  })
-                  .then(data => {
-                    let p = document.getElementById("list")
-                    let c = document.getElementById(`list${listId}`);
-                    p.removeChild(c);
-                  })
-                  .catch(error => console.error(error));
-                  location.reload();
-                  console.log(pay_load);
-              }
-              else {
-                console.log("Tarefa não editada");
-              }
-            }
-        
-                          
-                          
-                          
-                          
-                          
-        /*Alerta de Exclusão de Tarefa*/
-        deleteList = (listId) => {
-            let current = document.getElementById(`text${listId}`).innerHTML;
-            let deleteConfirm = confirm(`Tem certeza que deseja excluir a Tarefa? ${current}`);
-            const url = 'https://todolist-api.edsonmelo.com.br/api/task/delete/';
-            const headers = { 'Content-type': 'application/json', 'Authorization': token };
-            const pay_load = {
-                id: listId
-            };
-            
-            if (deleteConfirm) {
-          
-              fetch(url, {
-                method: 'DELETE',
-                headers: headers,
-                body: JSON.stringify(pay_load)
-              })
-                .then(response => {
-                  if (!response.ok) {
-                    throw new Error(response.status);
-                  }
-                  return response.json();
-                })
-                .then(data => {
-                  let p = document.getElementById("list")
-                  let c = document.getElementById(`list${listId}`);
-                  p.removeChild(c);
-                })
-                .catch(error => console.error(error));
-                console.log(pay_load);
+                // limpar o input e atualizar o número de tarefas
+                console.log(newTask);
+                input.value = "";
+
                 location.reload();
+            })
+            .catch(error => console.error(error));
+    }
+};
+
+/* Verificar se o item foi concluído */
+done = (listId) => {
+    let checkbox = document.getElementById(`check${listId}`);
+    let current = document.getElementById(`text${listId}`);
+    let classExit = current.classList.contains("text-decoration-line-through");
+    const url = 'https://todolist-api.edsonmelo.com.br/api/task/update/';
+    const headers = { 'Content-type': 'application/json', 'Authorization': token };
+    var pay_load = ""
+
+    if (classExit == true) {
+        current.classList.remove("text-decoration-line-through");
+        pay_load = {
+            id: listId,
+            name: current.innerText,
+            realized: 0,
+        };
+    } else {
+        current.classList.add("text-decoration-line-through");
+        pay_load = {
+            id: listId,
+            name: current.innerText,
+            realized: 1,
+        };
+    }
+
+    fetch(url, {
+        method: 'PUT',
+        headers: headers,
+        body: JSON.stringify(pay_load),
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response['message']);
             }
-            else {
-              console.log("Tarefa não excluída");
-            }
-          }
-
-
-        function logout(){
-
-            localStorage.clear();
-            window.location.href = '../index.html'
-            
+            return response.json();
+        })
+        .then(data => {
+           
+        })
+        .catch(error => console.error(error));
+    
+    
+}
+/*Verificar se tem mais de 3 caractéres */
+filterList = (x) => {
+    if (x) {
+        if (x.length >= minimalValue) {
+            return x;
         }
-            
+        else {
+            alert("Digite mais de 3 caractéres no nome da Tarefa")
+        }
+    }
+    else {
+        return false;
+    }
+}
+/*Alerta de Edição de Tarefa*/
+editList = (listId, listName, listDone) => {
+    let current = document.getElementById(`text${listId}`).innerHTML;
+    let newText = prompt(`Deseja alterar o nome da Tarefa? ${current}`);
+    const url = 'https://todolist-api.edsonmelo.com.br/api/task/update/';
+    const headers = { 'Content-type': 'application/json', 'Authorization': token };
+    const pay_load = {
+        id: listId,
+        name: newText,
+        realized: listDone,
+    };
+
+    if (filterList(newText)) {
+        fetch(url, {
+            method: 'PUT',
+            headers: headers,
+            body: JSON.stringify(pay_load),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(response.status);
+                }
+                return response.json();
+            })
+            .then(data => {
+                let p = document.getElementById("list")
+                let c = document.getElementById(`list${listId}`);
+                p.removeChild(c);
+            })
+            .catch(error => console.error(error));
+        location.reload();
+        console.log(pay_load);
+    }
+    else {
+        console.log("Tarefa não editada");
+    }
+}
+
+
+
+
+
+
+/*Alerta de Exclusão de Tarefa*/
+deleteList = (listId) => {
+    let current = document.getElementById(`text${listId}`).innerHTML;
+    let deleteConfirm = confirm(`Tem certeza que deseja excluir a Tarefa? ${current}`);
+    const url = 'https://todolist-api.edsonmelo.com.br/api/task/delete/';
+    const headers = { 'Content-type': 'application/json', 'Authorization': token };
+    const pay_load = {
+        id: listId
+    };
+
+    if (deleteConfirm) {
+
+        fetch(url, {
+            method: 'DELETE',
+            headers: headers,
+            body: JSON.stringify(pay_load)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(response.status);
+                }
+                return response.json();
+            })
+            .then(data => {
+                let p = document.getElementById("list")
+                let c = document.getElementById(`list${listId}`);
+                p.removeChild(c);
+            })
+            .catch(error => console.error(error));
+        console.log(pay_load);
+        location.reload();
+    }
+    else {
+        console.log("Tarefa não excluída");
+    }
+}
+
+
+function logout() {
+
+    localStorage.clear();
+    window.location.href = '../index.html'
+
+}
